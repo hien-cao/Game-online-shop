4dddc8d48b42c612e44ea1bc18526e241b86af02
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2019-01-10 13:29:24 +0200

    update fixes third party login

--- apps/user/utils/models.py
+++
@@ -1,6 +0,0 @@
-from ..models import Profile
-
-def save_profile(backend, user, response, *args, **kwargs):
-    if user.profile == None:
-        profile = Profile(user=user)
-        profile.save()
