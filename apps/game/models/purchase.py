6d181089e04d9e75c2f1423053d911501afcd0a5
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2019-01-10 21:27:20 +0200

    update purchase fields

--- apps/game/models/purchase.py
+++ apps/game/models/purchase.py
@@ -1,10 +1,17 @@
 import requests
+import os
+import uuid
 from django.db import models
 
 from ...user.models import Profile
 
+assert(os.environ['PAYMENT_API_URI'] is not None)
+assert(os.environ['PAYMENT_API_SELLER_ID'] is not None)
+assert(os.environ['PAYMENT_API_KEY'] is not None)
+
 class Purchase(models.Model):
-    purchased_at = models.DateField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    purchased_at = models.DateTimeField(auto_now_add=True, blank=True, editable=False)
     # set separate price field for purchases (as it may depending if there is an active discount)
     price = models.DecimalField(default=0, max_digits=6, decimal_places=2)
     game = models.ForeignKey(
@@ -17,7 +24,20 @@ class Purchase(models.Model):
     )
 
     def save(self):
+        self.price = self.game.price
         if True:
-            super().save()
+            # requests.post(
+            #     os.environ['PAYMENT_API_URI'],
+            #     data={
+            #         'pid': '',
+            #         'sid': os.environ['PAYMENT_API_SELLER_ID'],
+            #         'amount': self.price,
+            #         'success_url': '',
+            #         'cancel_url': '',
+            #         'error_url': '',
+            #         'checksum': '',
+            #     }
+            # )
+            super(Purchase, self).save()
         else:
             return
