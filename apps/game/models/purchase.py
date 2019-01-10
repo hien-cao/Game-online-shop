6528b0d106c49ff6d84c0dc4252391e128c7c2ed
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2019-01-10 20:47:24 +0200

    add price column to purchase

--- apps/game/models/purchase.py
+++ apps/game/models/purchase.py
@@ -1,9 +1,12 @@
+import requests
 from django.db import models
 
 from ...user.models import Profile
 
 class Purchase(models.Model):
     purchased_at = models.DateField()
+    # set separate price field for purchases (as it may depending if there is an active discount)
+    price = models.DecimalField(default=0, max_digits=6, decimal_places=2)
     game = models.ForeignKey(
         'Game',
         on_delete=models.CASCADE
@@ -12,3 +15,9 @@ class Purchase(models.Model):
         Profile,
         on_delete=models.CASCADE
     )
+
+    def save(self):
+        if True:
+            super().save()
+        else:
+            return
