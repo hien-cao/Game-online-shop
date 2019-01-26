fab579cc9cb56050c85e6e01f9a50b995ac22876
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2019-01-26 10:29:58 +0200

    add barebones homepage

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,16 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
-from .contexts import home_context
-
 
 def home(request):
     if request.user.is_authenticated:
         return HttpResponseRedirect('games')
-    context = {
-        **home_context,
-    }
-    return HttpResponse('Hello world!')
+    return render(request, 'home.html')
 
 
 def terms_and_conditions(request):
