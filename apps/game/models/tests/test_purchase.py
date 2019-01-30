d964e7074fc44a06523730eb3c66ffddcf40f759
Juho Jokela <juho.jokela@atalent.fi>
2019-01-30 11:40:45 +0200

    Half of the tests for Purchase model

---
+++ apps/game/models/tests/test_purchase.py
@@ -0,0 +1,65 @@
+from django.utils import timezone
+from django.test import TestCase
+
+from ...models import Game, Purchase
+from ....user.utils.mock_user import create_mock_user
+
+DUMMY_PID = "abc123"
+DUMMY_REF = 1
+DUMMY_RESULT = "success"
+
+
+class PurchaseTestCase(TestCase):
+    def setUp(self):
+        # create users
+        self.game_creator = create_mock_user(0)
+        self.game_purchaser = create_mock_user(1)
+        # create a game to purchase
+        self.game = Game.objects.create(
+            name="game_0",
+            created_by=self.game_creator.profile,
+            price=1,
+        )
+        self.purchase = Purchase.objects.create(
+            price=self.game.price,
+            game=self.game,
+            created_by=self.game_purchaser.profile,
+        )
+
+    def test_purchase_can_be_created(self):
+        """Create a purchase and assert."""
+        purchase = Purchase.objects.create(
+            price=self.game.price,
+            game=self.game,
+            created_by=self.game_purchaser.profile,
+        )
+        self.assertIsInstance(purchase, Purchase)
+
+    def test_purchase_can_be_updated(self):
+        """Try to update, refresh and assert."""
+        purchase_timestamp = timezone.now()
+        self.purchase.purchased_at = purchase_timestamp
+        self.purchase.ref = DUMMY_REF
+        self.purchase.save()
+        self.purchase.refresh_from_db()
+        self.assertIsInstance(self.purchase, Purchase)
+        self.assertEqual(self.purchase.ref, DUMMY_REF)
+        self.assertEqual(self.purchase.purchased_at, purchase_timestamp)
+
+    def test_property_pid(self):
+        """See that pid property exists and does not contain '-'"""
+        self.assertIsNotNone(self.purchase.pid)
+        self.assertNotIn(self.purchase.pid, "-")
+
+    def test_payment_context(self):
+        """See that payment context is contains all key, value pairs"""
+        context = self.purchase.get_payment_context()
+        self.assertIsNotNone(context)
+        for _, value in context.items():
+            self.assertIsNotNone(value)
+
+    def test_activate(self):
+        pass
+
+    def test_cancel(self):
+        pass
