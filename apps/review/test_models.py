7d5d040d20d370cdfca253f26704d458cd0be9f3
Juho Jokela <juho.jokela@atalent.fi>
2019-01-31 14:49:53 +0200

    Auto stash before cherrypick of "Fix warning from datetime.now()"

---
+++ apps/review/test_models.py
@@ -0,0 +1,36 @@
+from django.utils import timezone
+from django.test import TestCase
+
+from ..game.models import Game, Purchase
+from ..user.utils.mock_user import create_mock_user
+
+DUMMY_REF = 1
+
+class ReviewTestCase(TestCase):
+    def setUp(self):
+        # Create users
+        self.game_creator = create_mock_user(0)
+        self.purchaser_and_reviewer = create_mock_user(1)
+        # Create a game
+        self.game = Game.objects.create(
+            name="game",
+            created_by=self.game_creator.profile,
+            price=0
+        )
+        # Create a purchase
+        self.purchase = Purchase.objects.create(
+            game=self.game,
+            price=self.game.price,
+            created_by=self.purchaser_and_reviewer.profile,
+            purchased_at=timezone.now(),
+            ref=DUMMY_REF
+        )
+
+    def tearDown(self):
+        pass
+
+    def test_review_can_be_created_if_purchased(self):
+        pass
+
+    def test_review_cannot_be_created_if_no_purchase(self):
+        pass
