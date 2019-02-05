895541433f49a69d95c0307276d62c7444238b11
Juho Jokela <juho.jokela@atalent.fi>
2019-02-05 14:14:05 +0200

    Suggestions for api endpoints that need to be implemented

--- apps/apis/dev_api/urls.py
+++ apps/apis/dev_api/urls.py
@@ -5,7 +5,7 @@ from .views import generate, games, game, my_reviews, game_reviews
 urlpatterns = [
     path('generate/', generate, name='dev-api-generate'),
     path('games/', games, name='dev-api-games'),
-    path('games/<int:game_id>', game, name='dev-api-game'),
+    path('games/<int:game_id>/', game, name='dev-api-game'),
     path('my-reviews/', my_reviews, name='dev-api-my-reviews'),
     path('game-reviews/', game_reviews, name='dev-api-game-reviews'),
 ]
