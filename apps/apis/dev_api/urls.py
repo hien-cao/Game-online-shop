a1b790f742d58244fc69a9071973b6095fd4d339
Hien Cao <hien.cao@aalto.fi>
2019-02-05 20:40:13 +0200

    Add two api requests - game statistics and individual game review

--- apps/apis/dev_api/urls.py
+++ apps/apis/dev_api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import include, path
 
-from .views import generate, games, game, my_reviews, game_reviews
+from .views import generate, games, game, my_reviews, game_reviews, game_review, game_statistics
 
 urlpatterns = [
     path('generate/', generate, name='dev-api-generate'),
     path('games/', games, name='dev-api-games'),
     path('games/<int:game_id>/', game, name='dev-api-game'),
+    path('game-statistics/<int:game_id>/', game_statistics, name='dev-api-game-statistics'),
     path('my-reviews/', my_reviews, name='dev-api-my-reviews'),
     path('game-reviews/', game_reviews, name='dev-api-game-reviews'),
+    path('game-reviews/<int:game_id>/', game_review, name='dev-api-game-review'),
 ]
