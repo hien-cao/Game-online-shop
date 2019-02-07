abb8c06c6953c46cffe7ed258b98dd68d121d2b5
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2019-02-07 17:01:34 +0200

    add event listener to game delete button

--- src/games/details.js
+++ src/games/details.js
@@ -1,20 +1,46 @@
+/* global gameID */
 import {defaultFetch} from '../util.js';
 
 (() => {
-  const deleteReviewBtn = document.getElementById("delete_review");
-  const onDeleteClick = (e) => {
-    defaultFetch(`/games/${gameID}/review/delete/`, undefined, {method: "DELETE"})
-      .then(response => response.json())
-      .then(response => {
-        if (response.message == "success") {
-          deleteReviewBtn.parentNode.parentNode.remove();
-        } else {
-          deleteReviewBtn.innerText = response.message;
-          deleteReviewBtn.classList.add("disabled");
-          deteleReviewBtn.removeEventListener("click", onDeleteClick);
-        }
-      });
+  const deleteReviewBtn = document.getElementById('delete-review');
+  const deleteGameBtn = document.getElementById('delete-game');
+
+  const onReviewDelete = (e) => {
+    defaultFetch(
+        `/games/${gameID}/review/delete/`,
+        undefined, {method: 'DELETE'}
+    )
+        .then((response) => response.json())
+        .then((response) => {
+          if (response.message == 'success') {
+            deleteReviewBtn.parentNode.parentNode.remove();
+          } else {
+            deleteReviewBtn.innerText = response.message;
+            deleteReviewBtn.classList.add('disabled');
+            deleteReviewBtn.removeEventListener('click', onReviewDelete);
+          }
+        });
   };
 
-  deleteReviewBtn.addEventListener("click", onDeleteClick)
+  const onGameDelete = (e) => {
+    if (window.confirm('are you sure you want to delete the game? The operation cannot be cancelled')) {
+      defaultFetch(
+          `/games/${gameID}/edit/`,
+          undefined,
+          {method: 'DELETE'}
+      )
+          .then((response) => {
+            if (response.ok) {
+              window.location.href = '/games/uploads/';
+            }
+          });
+    }
+  };
+
+  if (deleteReviewBtn) {
+    deleteReviewBtn.addEventListener('click', onReviewDelete);
+  }
+  if (deleteGameBtn) {
+    deleteGameBtn.addEventListener('click', onGameDelete);
+  }
 })();
