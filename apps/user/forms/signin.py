d772680013f88884790423fcc1297526e026594b
Juho Jokela <juho.jokela@atalent.fi>
2019-02-08 14:35:25 +0200

    Remove unused imports

--- apps/user/forms/signin.py
+++ apps/user/forms/signin.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.models import User
+
 
 class SignInForm(AuthenticationForm):
     def confirm_login_allowed(self, user):
