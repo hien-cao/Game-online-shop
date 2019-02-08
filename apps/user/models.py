35bc684a72a9ac949b1b0429d2fc2d9cbae03141
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2019-02-08 13:33:59 +0200

    comment user model

--- apps/user/models.py
+++ apps/user/models.py
@@ -5,12 +5,22 @@ from django.dispatch import receiver
 
 
 class Profile(models.Model):
+    """
+    Profile model to provide additional properties to Djangos abstract user
+    model
+    """
     user = models.OneToOneField(
         User,
         on_delete=models.CASCADE,
         primary_key=True
     )
+    # Profile.is_developer has to be true for the user to be able to
+    # add games and access dev api
     is_developer = models.BooleanField(default=False)
+    # Separate field to track email confirmation status. Even though the
+    # Django user model provides 'active' property, we may want to be able
+    # to deactivate accounts (banning) even though their emails will remain
+    # confirmed
     email_confirmed = models.BooleanField(default=False)
 
     def __str__(self):
@@ -18,9 +28,10 @@ class Profile(models.Model):
 
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
-        if created:
+        if created:  # Create profile for user when user is created
             Profile.objects.create(user=instance)
 
+    # whenever a user instance is saved, will also save profile linked to it
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
